a6f312a23d9643e39ba64819d36fd99e6cd750a4
devsxis <devsxis@gmail.com>
2024-06-25 12:31:47 +0500

    text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ipfs with fleek.
+          Ipfs with fleek..
         </a>
       </header>
     </div>
